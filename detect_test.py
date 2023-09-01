209af193121dba50866d3908bcd417aaa9c4f52f
Joannaaaaaa <98182630+Joannaaaaaa@users.noreply.github.com>
2023-09-01 18:34:51 +0800

    Update detect_test.py

--- detect_test.py
+++ detect_test.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("runs/detect/train/weights/best.pt")  # training weight
 result = model.predict(
-    source="000000.jpg",
+    source="000000.jpg",  # test image
     mode="predict",
     save=True,
     device=0
